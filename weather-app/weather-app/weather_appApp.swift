05a845c21d48737038ba08dc65382db9680fd13f
Jean Paul Massoud <jean.paul@devies.se>
2024-05-28 14:33:44 +0200

    initial commit

---
+++ weather-app/weather-app/weather_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  weather_appApp.swift
+//  weather-app
+//
+//  Created by Jean paul Massoud on 2024-05-28.
+//
+
+import SwiftUI
+
+@main
+struct weather_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
